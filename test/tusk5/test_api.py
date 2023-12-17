a02dbf73dc3154096e9cb3d30f28f814f63fba6f
BapeHuKcJloByLLlKepoM <148371058+BapeHuKcJloByLLlKepoM@users.noreply.github.com>
2023-12-17 19:26:04 +0500

    Update test_api.py

--- test/tusk5/test_api.py
+++ test/tusk5/test_api.py
@@ -1,12 +1,25 @@
 import requests
 
-url = "https://dataen-mvfulkp9imtqm24v4zhmpy.streamlit.app/"
-timeout = 5  # Установите желаемое значение таймаута в секундах
+API_URL = "https://dataen-mvfulkp9imtqm24v4zhmpy.streamlit.app/"
+ENDPOINT = "/synthesize_tts"
 
-try:
-    response = requests.get(url, timeout=timeout)
-    # Далее обрабатывайте ответ сервера...
-except requests.exceptions.Timeout:
-    print("Таймаут запроса. Проверьте подключение к серверу.")
-except requests.exceptions.RequestException as e:
-    print("Ошибка во время запроса:", e)
+def test_synthesize_tts():
+    # Тест 1: проверка работоспособности API
+    response = requests.get(API_URL + "/healthcheck")
+    assert response.status_code == 200
+
+    # Тест 2: Запрос на озвучивание текста
+    payload = {
+        "text": "Hello, world!",
+        "voice": "en-US"
+    }
+    response = requests.post(API_URL + ENDPOINT, json=payload)
+    assert response.status_code == 200
+
+    # Тест 3: Проверьте содержимое ответа
+    data = response.json()
+    assert "audio" in data
+    assert isinstance(data["audio"], str)
+
+if __name__ == "__main__":
+    test_synthesize_tts()
