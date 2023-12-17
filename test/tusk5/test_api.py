c52cda1f71279153edaa9520f3a209efd6eabe37
BapeHuKcJloByLLlKepoM <148371058+BapeHuKcJloByLLlKepoM@users.noreply.github.com>
2023-12-18 00:59:09 +0500

    Update test_api.py

--- test/tusk5/test_api.py
+++ test/tusk5/test_api.py
@@ -1,7 +1,7 @@
 import requests
 
 API_URL = "http://34.125.57.194:8501"
-ENDPOINT = "/synthesize_tts"
+#ENDPOINT = "/synthesize_tts"
 
 def test_synthesize_tts():
     # Test case 1: Check if the API is up and running
@@ -13,7 +13,8 @@ def test_synthesize_tts():
         "text": "Hello, world!",
         "voice": "en-US"
     }
-    response = requests.post(API_URL + ENDPOINT, json=payload)
+    response = requests.post(API_URL, json=payload)
+    #response = requests.post(API_URL + ENDPOINT, json=payload)
     assert response.status_code == 200
 
     # Test case 3: Check the response content
