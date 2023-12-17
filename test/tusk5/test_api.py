a35d2d89df96a7ecfa77de72f73501ada74204bd
BapeHuKcJloByLLlKepoM <148371058+BapeHuKcJloByLLlKepoM@users.noreply.github.com>
2023-12-17 20:08:20 +0500

    Update test_api.py

--- test/tusk5/test_api.py
+++ test/tusk5/test_api.py
@@ -1,25 +1,19 @@
+import unittest
 import requests
 
-API_URL = "https://dataen-mvfulkp9imtqm24v4zhmpy.streamlit.app/"
-ENDPOINT = "/synthesize_tts"
+class TestMmsTtsAPI(unittest.TestCase):
+    def test_synthesize_text(self):
+        text = "Hello, how are you?"
+        response = requests.post("https://appapppy-glg9ykmarcwvxrb4agbxyd.streamlit.app/synthesize_text", json={"text": text})
 
-def test_synthesize_tts():
-    # Тест 1: проверка работоспособности API
-    response = requests.get(API_URL + "/healthcheck")
-    assert response.status_code == 200
+        # Проверяем статус код ответа
+        self.assertEqual(response.status_code, 200)
+        
+        # Проверяем, что поле 'audio' присутствует в ответе
+        self.assertIn('audio', response.json())
+        
+        # Проверяем, что поле 'audio' не пустое
+        self.assertNotEqual(response.json()['audio'], '')
 
-    # Тест 2: Запрос на озвучивание текста
-    payload = {
-        "text": "Hello, world!",
-        "voice": "en-US"
-    }
-    response = requests.post(API_URL + ENDPOINT, json=payload)
-    assert response.status_code == 200
-
-    # Тест 3: Проверьте содержимое ответа
-    data = response.json()
-    assert "audio" in data
-    assert isinstance(data["audio"], str)
-
-if __name__ == "__main__":
-    test_synthesize_tts()
+if __name__ == '__main__':
+    unittest.main()
