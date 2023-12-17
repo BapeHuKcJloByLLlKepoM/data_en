1b1881a6bc2bc19043a45177dd32a40998bb77b9
BapeHuKcJloByLLlKepoM <148371058+BapeHuKcJloByLLlKepoM@users.noreply.github.com>
2023-12-18 01:12:06 +0500

    Update test_short.py

--- test/tusk5/test_short.py
+++ test/tusk5/test_short.py
@@ -14,14 +14,10 @@ def app_url():
     return "https://dataen-mvfulkp9imtqm24v4zhmpy.streamlit.app/"
 
 def test_translation(driver, app_url):
-
-   
-
-
     driver.get(app_url)
-    text_input = driver.find_element(By.XPATH, '//input[@type="text"]')
-    text_input.send_keys("Hello, World!" + Keys.ENTER)
-    driver.implicitly_wait(5)
+    #text_input = driver.find_element(By.XPATH, '//input[@type="text"]')
+    #text_input.send_keys("Hello, World!" + Keys.ENTER)
+    driver.implicitly_wait(1000)
     audio_element = driver.find_element_by_xpath('//audio')
     assert audio_element.is_displayed()
 
