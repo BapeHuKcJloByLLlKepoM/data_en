01040e73f7d5571b7739d553b297ecac6f2d0489
BapeHuKcJloByLLlKepoM <organismus22@gmail.com>
2023-12-22 03:37:08 +0500

    tx add

---
+++ tx.py
@@ -0,0 +1,24 @@
+from transformers import VitsModel, AutoTokenizer
+import torch
+
+model = VitsModel.from_pretrained("facebook/mms-tts-eng")
+tokenizer = AutoTokenizer.from_pretrained("facebook/mms-tts-eng")
+
+text = "welcome to the cum zone"
+
+inputs = tokenizer(text, return_tensors="pt")
+
+with torch.no_grad():
+    output = model(**inputs).waveform[0]
+
+#сохранение  wav
+
+import scipy
+
+<put.float().numpy())
+
+# отображение аудио сразу 
+
+from IPython.display import Audio
+
+Audio(output.numpy(), rate=model.config.sampling_rate)
