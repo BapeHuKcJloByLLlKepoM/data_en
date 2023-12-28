71c54adc1e306086d7e1db6b515a4f2823664018
BapeHuKcJloByLLlKepoM <148371058+BapeHuKcJloByLLlKepoM@users.noreply.github.com>
2023-12-28 18:23:41 +0500

    Update test_short.py

--- всякие тесты для стримлита/Новая папка (2)/test_short.py
+++ всякие тесты для стримлита/Новая папка (2)/test_short.py
@@ -1,24 +1,24 @@
-import pytest
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
+# import pytest
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.common.keys import Keys
 
 
-@pytest.fixture(scope="module")
-def driver():
-    driver = webdriver.Chrome()
-    yield driver
-    driver.quit()
+# @pytest.fixture(scope="module")
+# def driver():
+#     driver = webdriver.Chrome()
+#     yield driver
+#     driver.quit()
 
-@pytest.fixture(scope="module")
-def app_url():
-    return "https://dataen-mvfulkp9imtqm24v4zhmpy.streamlit.app/"
+# @pytest.fixture(scope="module")
+# def app_url():
+#     return "https://dataen-mvfulkp9imtqm24v4zhmpy.streamlit.app/"
 
-def test_translation(driver, app_url):
-    driver.get(app_url)
-    text_input = driver.find_element(By.XPATH, '//input[@type="text"]')
-    text_input.send_keys("Hello, World!" + Keys.ENTER)
-    driver.implicitly_wait(1000)
-    audio_element = driver.find_element_by_xpath('//audio')
-    assert audio_element.is_displayed()
+# def test_translation(driver, app_url):
+#     driver.get(app_url)
+#     text_input = driver.find_element(By.XPATH, '//input[@type="text"]')
+#     text_input.send_keys("Hello, World!" + Keys.ENTER)
+#     driver.implicitly_wait(1000)
+#     audio_element = driver.find_element_by_xpath('//audio')
+#     assert audio_element.is_displayed()
 
